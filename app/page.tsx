defa8955b52ba7dff59e8ae229a30ee739f5cbfd
Cheesypasta1 <trivedi.dhyey@yahoo.co.uk>
2025-02-01 22:41:58 +0000

    started loading animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
 import VideoPane from "./components/VideoPane"
 import InteractivePane from "./components/InteractivePane"
+import { useState } from "react"
+import { LoadingText } from "@/components/loading_text"
 
 export default function Home() {
+  const isLoading = useState<boolean>(true)
+  if (isLoading) {
+    return <LoadingText />
+  }
+
   return (
     <PanelGroup direction="horizontal">
       <Panel defaultSize={50} minSize={30}>
