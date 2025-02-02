f7e2be65bbc445a35a486441c6a096874cadda41
nb1622 <nb1622@ic.ac.uk>
2025-02-02 04:46:57 +0000

    Updated reco pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,6 @@ export default function Navbar() {
         <a href="/" className="text-orange-600 text-3xl font-bold tracking-tight">
           VisuMath
         </a>
-        {/* Button aligned to right */}
-        <a
-          href="#get-started"
-          className="bg-orange-600 hover:bg-orange-500 text-white font-medium py-2 px-4 rounded-md text-lg absolute right-4"
-        >
-          Contact
-        </a>
       </div>
     </nav>
   );
